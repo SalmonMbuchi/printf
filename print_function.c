fd6cf80bc7dae0d0c88b6b485ccceca28fe92f17
njorogegatua <gatuapnjoroge@gmail.com>
2022-05-21 10:35:38 -0700

    task 0

--- print_function.c
+++ print_function.c
@@ -4,12 +4,46 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *  - takes two arguments
- * @format: a character string
- * @argz: variable of type va_lise
- *Return: void
+ * print_out - checks specifiers
+ * @format: character string
+ * @print_arr: an arrar of struct
+ * @argz: va_list variable
+ * Return: void
  */
+int print_out(const char *format, format_ted *print_arr, va_list argz)
+{
+	char a;
+	int digit = 0, b = 0, c = 0;
 
+	a = format[b];
+	while (a != '\0')
+	{
+		if(a == '%')
+		{
+			c = 0;
+			b++;
+			a = format[b];
+			while (print_arr[c].type !=NULL &&
+				a != *(print_arr[c].type))
+				c++;
+			if (print_arr[c].type !=NULL)
+				digit = digit + print_arr[c].f(argz);
+			else
+			{
+				if (a == '\0')
+					return (-1);
+				if (a != '%')
+					digit += _putchar('%');
+				digit += _putchar(a);
+			}
+		}
+		else
+			digit += _putchar(a);
+		b++;
+		a = format[b];
+	}
+	return (digit);
+}
 /**
  * _printf - produces ouput according to a format
  * @format: character string
